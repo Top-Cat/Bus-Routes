36d299e7c6917307492ca7c454e9b9033db36565
Thomas Cheyney <webmaster@thomasc.co.uk>
2012-09-07 15:53:28 +0100

    Removed development parameter. Added analytics.

--- web/index.php
+++ web/index.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
 	<head profile="http://gmpg.org/xfn/11">
-		<link rel="stylesheet" href="style.css?t=<?php print gmdate("U"); ?>"/>
+		<link rel="stylesheet" href="style.css"/>
 		<script type="text/javascript" src="jquery.js"></script>
 		<script type="text/javascript" src="easing.js"></script>
 		<script type="text/javascript" src="script.js"></script>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<title>Bus Route Finder</title>
+		<script type="text/javascript">
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-34622141-1']);
+			_gaq.push(['_trackPageview']);
+			(function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+		</script>
 	</head>
 	<body><div><span>Bus Route Finder</span>Enter registration: <input /><strong>&nbsp;</strong><button><font><i><b>&nbsp;</b><u>Go</u></i></font></button><hr /><s></s></div><p>Web Design &copy; 2012 Thomas Cheyney</p></body>
 </html>
